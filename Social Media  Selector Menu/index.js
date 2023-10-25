11ee9d2ac0cc7049fad75ca843358e81b7254b2d
JosueHerazo <136448637+JosueHerazo@users.noreply.github.com>
2023-10-24 22:25:12 -1200

    Add files via upload

---
+++ Social Media  Selector Menu/index.js
@@ -0,0 +1,26 @@
+const menuEl = document.querySelector(".menu")
+const menuTextEL = document.querySelector(".menu p")
+
+const socialListEl  = document.querySelector
+(".social-list")
+
+const liEls = document.querySelectorAll
+(".social-list li")
+
+
+menuEl.addEventListener("click", () => {
+    socialListEl.classList.toggle("hide")
+    menuEl.classList.toggle("rotate")
+})
+
+liEls.forEach(liEl=>{
+    liEl.addEventListener("click", ()=> {
+        menuTextEL.innerHTML = liEl.innerHTML
+        socialListEl.classList.add("hide")
+        menuEl.classList.toggle("rotate")
+
+
+    })
+})
+
+ 
